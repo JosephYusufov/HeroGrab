df6f8a829071361ee5bfdcdf93cf97cc654797b0
Joseph Yusufov <jyusufov00@stuy.edu>
2020-10-16 10:58:22 -0400

    changes

--- src/heroGrab.js
+++ src/heroGrab.js
@@ -1,20 +1,23 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 const request = require('request');
-// const https = require('https');
 const rmrf = require('rmrf');
 
-const getLogoAndHeroImages = siteURL => {
+const getLogoAndHeroImages = (siteURL, browser) => {
 
     let URLObject = new URL(siteURL);
     let hostname = URLObject.hostname;
     let logo = {};
     let heroCollection = [];
     let images = [];
-    return puppeteer.launch().then(browser => {
-        return browser.newPage();        
-    }).then(page => {
-        return page.goto(siteURL).then(() => {
+
+    // let browser;
+
+    return browser.newPage().then(page => {
+        return page.goto(siteURL)
+        .then(() => {
+            return page.setDefaultNavigationTimeout(0);
+        }).then(() => {
             return page.$$('img');
         }).then(imagesArr => {
             return Promise.all(imagesArr.map(img => {
@@ -22,51 +25,124 @@ const getLogoAndHeroImages = siteURL => {
                     images.push(imgProps);
                 })
             }));
-        });
-    }).then(() => {
-        // console.log(images);
-        logo = images.filter(img => {
-            const logoPattern = RegExp('logo');
-            return(logoPattern.test(img.url));
-        });
+        }).catch(e => {
+            console.log(e);
+        }).then(() => {
+            return page.close();
+        }).then(() => {
+            // console.log(images);
+            logo = images.filter(img => {
+                const logoPattern = RegExp('logo');
+                return(logoPattern.test(img.url));
+            });
 
-        heroCollection = [...images];
-        heroCollection.sort((img1, img2) => {
-            const img1Area = img1.width * img1.height;
-            const img2Area = img2.width * img2.height;
-            if(img1Area > img2Area){
-                return -1;
-            } else {
-                return 1;
-            };
-        });
+            heroCollection = [...images];
+            heroCollection.sort((img1, img2) => {
+                const img1Area = img1.width * img1.height;
+                const img2Area = img2.width * img2.height;
+                if(img1Area > img2Area){
+                    return -1;
+                } else {
+                    return 1;
+                };
+            });
 
-        heroCollection = heroCollection.filter((img, i) => {
-            let notSquare = img.width != img.height;
-            let notDuplicate;
-            if(i === 0){
-                notDuplicate = true;
+            heroCollection = heroCollection.filter((img, i) => {
+                let notSquare = img.width != img.height;
+                let notDuplicate;
+                if(i === 0){
+                    notDuplicate = true;
+                } else {
+                    notDuplicate = !(heroCollection[i-1].url == heroCollection[i].url);
+                }
+                return(notSquare 
+                    && 
+                    notDuplicate
+                    );
+            });
+
+            heroCollection = [heroCollection[0], heroCollection[1], heroCollection[2]];
+            // console.log(heroCollection)
+
+        }).then(() => {
+            if(logo[0]){
+                logo = logo[0];
             } else {
-                notDuplicate = !(heroCollection[i-1].url == heroCollection[i].url);
+                logo = null;
             }
-            return(notSquare 
-                && 
-                notDuplicate
-                );
-        });
-
-        heroCollection = [heroCollection[0], heroCollection[1], heroCollection[2]];
-        // console.log(heroCollection)
-
-    }).then(() => {
-        if(logo[0]){
-            logo = logo[0];
-        } else {
-            logo = null;
-        }
-        return { "logo": logo, "heroCollection": heroCollection, "hostname": hostname };
-    })
+            return { "logo": logo, "heroCollection": heroCollection, "hostname": hostname };
+        })
+    });
 };
+
+
+// // Using async and await keywords
+// const getLogoAndHeroImages = async siteURL => {
+
+//     let URLObject = new URL(siteURL);
+//     let hostname = URLObject.hostname;
+//     let logo = {};
+//     let heroCollection = [];
+//     let images = [];
+
+//     let browser = await puppeteer.launch();
+//     let page = browser.newPage();
+
+//     await page.goto(url);
+
+//     let imgElements = await page.$$('img');
+//     await Promise.all(imgElements.map(img => {
+//         let imgObj;
+//         return img.evaluate(node => { 
+//             imgObj = {'url': node.src, 'width': node.naturalWidth, 'height': node.naturalHeight}
+//         }).then(() => {
+//             images.push(imgObj);
+//         });
+//     }));
+//     await browser.close();
+
+
+//     logo = images.filter(img => {
+//         const logoPattern = RegExp('logo');
+//         return(logoPattern.test(img.url));
+//     });
+
+//     heroCollection = [...images];
+//     heroCollection.sort((img1, img2) => {
+//         const img1Area = img1.width * img1.height;
+//         const img2Area = img2.width * img2.height;
+//         if(img1Area > img2Area){
+//             return -1;
+//         } else {
+//             return 1;
+//         };
+//     });
+
+//     heroCollection = heroCollection.filter((img, i) => {
+//         let notSquare = img.width != img.height;
+//         let notDuplicate;
+//         if(i === 0){
+//             notDuplicate = true;
+//         } else {
+//             notDuplicate = !(heroCollection[i-1].url == heroCollection[i].url);
+//         }
+//         return(notSquare 
+//             && 
+//             notDuplicate
+//             );
+//     });
+
+//     heroCollection = [heroCollection[0], heroCollection[1], heroCollection[2]];
+
+//     if(logo[0]){
+//         logo = logo[0];
+//     } else {
+//         logo = null;
+//     }
+//     return { "logo": logo, "heroCollection": heroCollection, "hostname": hostname };
+// }
+
+
 
 const saveImagesToDisk = imagesObj => {
     // const fullUrl = url;
@@ -84,19 +160,29 @@ const saveImagesToDisk = imagesObj => {
     fs.mkdirSync(`${dirPath}/hero`);
 
     if(imagesObj.logo){
-        let logoExtension = imagesObj.logo.url.substring(imagesObj.logo.url.lastIndexOf('.') + 1);
-        logoExtension = logoExtension.includes('?')? logoExtension.substring(0, logoExtension.lastIndexOf('?')): logoExtension;
-        download(imagesObj.logo.url, `${dirPath}/logo/logo.${logoExtension}`, function(){
-            // console.log('done');
-        });      
+        try{
+            let logoExtension = imagesObj.logo.url.substring(imagesObj.logo.url.lastIndexOf('.') + 1);
+            logoExtension = logoExtension.includes('?')? logoExtension.substring(0, logoExtension.lastIndexOf('?')): logoExtension;
+            download(imagesObj.logo.url, `${dirPath}/logo/logo.${logoExtension}`, function(){
+                // console.log('done');
+            });      
+        } catch(e){
+            console.log(e);
+        }
     }
 
     imagesObj.heroCollection.map((img, i) => {
-        let heroExtension = img.url.substring(img.url.lastIndexOf('.') + 1);
-        heroExtension = heroExtension.includes('?')? heroExtension.substring(0, heroExtension.lastIndexOf('?')): heroExtension;
-        download(img.url, `${dirPath}/hero/hero-${i}.${heroExtension}`, function(){
-            // console.log('done');
-        });
+        if(img){
+            try{
+                let heroExtension = img.url.substring(img.url.lastIndexOf('.') + 1);
+                heroExtension = heroExtension.includes('?')? heroExtension.substring(0, heroExtension.lastIndexOf('?')): heroExtension;
+                download(img.url, `${dirPath}/hero/hero-${i}.${heroExtension}`, function(){
+                    // console.log('done');
+                });
+            } catch(e){
+                console.log(e);
+            }
+        }
     })
 };
 
